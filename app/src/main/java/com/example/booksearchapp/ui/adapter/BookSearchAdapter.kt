1833508abdd4337e76a68ee00c3ee9b0304b5de2
이정훈 <xorolx297@naver.com>
2022-05-30 15:58:56 +0900

    From unversioned Files To versioned Files

---
+++ app/src/main/java/com/example/booksearchapp/ui/adapter/BookSearchAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.booksearchapp.ui.adapter
+
+import android.widget.ListAdapter
+import com.example.booksearchapp.data.model.Book
+
+class BookSearchAdapter : ListAdapter<Book, BookSearchViewHolder>() {
+
+    companion object {
+        
+    }
+}
